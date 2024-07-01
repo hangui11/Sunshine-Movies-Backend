561607995231626a945dc2570aecfe16834085e4
hang.yao <hang.yao@estudiantat.upc.edu>
2024-07-01 19:49:42 +0200

    add backend

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import numpy as np
+import pandas as pd
+import json
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/users")
+async def root():
+    df = pd.read_csv('./users.csv')
+    data = []
+    for i, row in df.iterrows():
+        userId = row['userId']
+        userName = row['userName']
+        password = row['password']
+        role = row['role']
+        data.append({'userId': userId, 'username': userName, 'password': password, 'role': role})
+    return data
