9a992a1f342ee912c0073d8865292cdec0529864
hangui11 <hangyao2002@gmail.com>
2024-07-08 11:16:26 +0200

    v

--- api/main.py
+++ api/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-import numpy as np
-import pandas as pd
-import json
+# import numpy as np
+# import pandas as pd
+# import json
 app = FastAPI()
 
 app.add_middleware(
@@ -20,14 +20,14 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/users")
-async def root():
-    df = pd.read_csv('../users.csv')
-    data = []
-    for i, row in df.iterrows():
-        userId = row['userId']
-        userName = row['userName']
-        password = row['password']
-        role = row['role']
-        data.append({'userId': userId, 'username': userName, 'password': password, 'role': role})
-    return data
+# @app.get("/users")
+# async def root():
+#     df = pd.read_csv('../users.csv')
+#     data = []
+#     for i, row in df.iterrows():
+#         userId = row['userId']
+#         userName = row['userName']
+#         password = row['password']
+#         role = row['role']
+#         data.append({'userId': userId, 'username': userName, 'password': password, 'role': role})
+#     return data
