dba03903412229dd50bedc79196b7930fe1dd54a
hang.yao <hang.yao@estudiantat.upc.edu>
2024-09-03 22:09:44 +0200

    fix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from typing import List
 from fastapi import FastAPI, HTTPException, Depends, Query
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 from trivial_recommender import Trivial
+from user_based_recommender import UserToUser
 import numpy as np
 import pandas as pd
 import json
@@ -22,27 +22,31 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/models")
-async def models(model: str, ratings: str = Query(...), users: str = Query(...), movies: str = Query(...)):
+# @app.get("/models")
+# async def models(model: str, user_id: str, ratings: str = Query(...), users: str = Query(...), movies: str = Query(...)):
 
-    ratings = json.loads(ratings)
-    users = json.loads(users)
-    movies = json.loads(movies)
-    model = json.loads(model)
+#     ratings = json.loads(ratings)
+#     users = json.loads(users)
+#     movies = json.loads(movies)
+#     model = json.loads(model)
+#     user_id = json.loads(user_id)
 
-    df_ratings = []
+#     df_ratings = []
 
-    # create a dataframe from the ratings list
-    for i in range(len(ratings)):
-        df_ratings.append([users[i], movies[i], ratings[i]])
+#     # create a dataframe from the ratings list
+#     for i in range(len(ratings)):
+#         df_ratings.append([users[i], movies[i], ratings[i]])
     
-    df = pd.DataFrame(df_ratings, columns=["userId", "movieId", "rating"])
+#     df = pd.DataFrame(df_ratings, columns=["userId", "movieId", "rating"])
 
 
-    # Start computations for the selected model
-    recommendation_movieId = []
-    if model == "Trivial":
-        trivial = Trivial(df)
-        recommendation_movieId = trivial.topMovieTrivial
-    print(recommendation_movieId)
-    return {"result": recommendation_movieId}
+#     # Start computations for the selected model
+#     recommendation_movieId = []
+#     if model == "Trivial":
+#         trivial = Trivial(df)
+#         recommendation_movieId = trivial.topMovieTrivial
+#     # if model == "User-to-User":
+#         # u2u = UserToUser(df, users)
+#         # recommendation_movieId = u2u.user_based_recommender(1)
+#     print(recommendation_movieId)
+#     return {"result": recommendation_movieId}
